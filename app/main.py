415fa748a2d7c64bf3ca291ab8b860a1f9230a3b
Aaron Carson <aaron@aaroncarson.co.uk>
2025-02-23 12:22:03 +0000

    genericise it a bit

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ import git
 import logfire
 from discord_webhook import DiscordEmbed, DiscordWebhook
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Request, BackgroundTasks
+from fastapi import BackgroundTasks, FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 
 config_path = "/data/options.json"
@@ -22,6 +22,11 @@ if os.path.exists(config_path):
         GITHUB_WEBHOOK_SECRET = config_data["github_webhook_secret"]
         NOTIFY_DISCORD_USER = config_data["notify_discord_user"]
         CADDY_CONTAINER_NAME = config_data["caddy_container_name"]
+        GIT_REPO_PATH = config_data["git_repo_path"]
+        GIT_SSH_KEY_FILE = config_data["git_ssh_key_file"]
+        GIT_SSH_KNOWN_HOSTS_FILE = config_data["git_ssh_known_hosts_file"]
+        GIT_REPO_URL = config_data["git_repo_url"]
+        GIT_REPO_BRANCH = config_data["git_repo_branch"]
 
 else:
     logfire.info("Config file not found, using environment variables")
@@ -32,6 +37,11 @@ else:
     GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET")
     NOTIFY_DISCORD_USER = os.getenv("NOTIFY_DISCORD_USER")
     CADDY_CONTAINER_NAME = os.getenv("CADDY_CONTAINER_NAME")
+    GIT_REPO_PATH = os.getenv("GIT_REPO_PATH")
+    GIT_SSH_KEY_FILE = os.getenv("GIT_SSH_KEY_FILE")
+    GIT_SSH_KNOWN_HOSTS_FILE = os.getenv("GIT_SSH_KNOWN_HOSTS_FILE")
+    GIT_REPO_URL = os.getenv("GIT_REPO_URL")
+    GIT_REPO_BRANCH = os.getenv("GIT_REPO_BRANCH")
 
 app = FastAPI()
 webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL)
@@ -42,13 +52,16 @@ logfire.instrument_fastapi(app, capture_headers=True)
 
 
 def send_discord_success(repo: git.Repo, prev_commit: str, new_commit: str):
-    logfire.debug("Posting success to Discord...", attributes={"new_commit": new_commit, "prev_commit": prev_commit})
+    logfire.debug(
+        "Posting success to Discord...",
+        attributes={"new_commit": new_commit, "prev_commit": prev_commit},
+    )
     if prev_commit == new_commit:
         logfire.info("No changes detected in Caddy repo")
         return
-    
+
     prev_commit_short = prev_commit[:7]
-    new_commit_short = new_commit[:7]  
+    new_commit_short = new_commit[:7]
     commits = list(repo.iter_commits(f"{new_commit}...{prev_commit}~1"))
     new_commit_msg = commits[0].message.splitlines()[0]
     prev_commit_msg = commits[-1].message.splitlines()[0]
@@ -74,21 +87,22 @@ def send_discord_success(repo: git.Repo, prev_commit: str, new_commit: str):
 
     embed.add_embed_field(
         name="Current commit",
-        value=f"{new_commit_msg} - [{new_commit_short}](https://github.com/caraar12345/infra-caddy/commit/{new_commit})",
+        value=f"{new_commit_msg} - [{new_commit_short}]({GIT_REPO_URL}/commit/{new_commit})",
         inline=False,
     )
 
     embed.add_embed_field(
         name="Previous commit",
-        value=f"{prev_commit_msg} - [{prev_commit_short}](https://github.com/caraar12345/infra-caddy/commit/{prev_commit})",
+        value=f"{prev_commit_msg} - [{prev_commit_short}]({GIT_REPO_URL}/commit/{prev_commit})",
         inline=False,
     )
 
     webhook.add_embed(embed)
     try:
-        webhook.execute()
+        webhook.execute(remove_files=True, remove_embeds=True)
     except Exception:
         logfire.error("Failed to post success to Discord", _exc_info=True)
+
 
 # Configuration
 def verify_signature(payload_body: bytes, signature_header: str) -> bool:
@@ -110,23 +124,23 @@ async def github_webhook(request: Request, background_tasks: BackgroundTasks):
         raise HTTPException(status_code=401, detail="Invalid signature")
     pprint.pprint(json.loads(payload_body))
     try:
-        # Perform git pull on /share/caddy directory
-        repo = git.Repo("/share/caddy")
+        # Perform git pull on CADDY_REPO_PATH directory
+        repo = git.Repo(GIT_REPO_PATH)
         prev_commit = repo.head.commit.hexsha
 
-        ssh_cmd = "ssh -i /ssl/.ssh/id_ed25519 -o UserKnownHostsFile=/ssl/.ssh/github.com.hostkey"
+        ssh_cmd = f"ssh -i {GIT_SSH_KEY_FILE} -o UserKnownHostsFile={GIT_SSH_KNOWN_HOSTS_FILE}"
         with repo.git.custom_environment(GIT_SSH_COMMAND=ssh_cmd):
             logfire.info("Pulling repo...")
-            repo.remotes["main"].pull()
+            repo.remotes[GIT_REPO_BRANCH].pull()
             logfire.debug("New commit: " + repo.head.commit.hexsha[:7])
 
         new_commit = repo.head.commit.hexsha
 
-        background_tasks.add_task(
-            send_discord_success, repo, prev_commit, new_commit
-        )
+        background_tasks.add_task(send_discord_success, repo, prev_commit, new_commit)
 
-        success_log = f"Successfully pulled latest changes from GitHub ({new_commit[:7]})"
+        success_log = (
+            f"Successfully pulled latest changes from GitHub ({new_commit[:7]})"
+        )
 
         if prev_commit != new_commit:
             # Send SIGHUP to the Docker container
@@ -135,7 +149,7 @@ async def github_webhook(request: Request, background_tasks: BackgroundTasks):
             container.kill(signal="SIGHUP")
             success_log += " and sent SIGHUP to container"
         else:
-            success_log += " but no changes detected in Caddy repo"
+            success_log += " but no changes detected in repo"
 
         logfire.info(success_log)
 
@@ -148,17 +162,16 @@ async def github_webhook(request: Request, background_tasks: BackgroundTasks):
             },
         )
     except Exception as e:
-        description = (
-            f"""<@!{NOTIFY_DISCORD_USER}> - Invalid Caddyfile.\n```\n{str(e)}\n```"""
-        )
+        description = f"""<@!{NOTIFY_DISCORD_USER}> - Error when pulling or reloading.\n```\n{str(e)}\n```"""
 
         embed = DiscordEmbed(
-            title="Caddy repo updated", description=description, color="ff005e"
+            title="Repo updated", description=description, color="ff005e"
         )
         webhook.add_embed(embed)
-        webhook.execute()
+        webhook.execute(remove_files=True, remove_embeds=True)
         logfire.error(
-            "Failed to pull latest changes from GitHub or send SIGHUP to container", _exc_info=True
+            "Failed to pull latest changes from GitHub or send SIGHUP to container",
+            _exc_info=True,
         )
 
         return JSONResponse(
